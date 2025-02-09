c86f7b95e96c5a78d79d0ab388b5df160418723f
fxrhannn <dangerfsgaming@gmail.com>
2025-02-09 23:17:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const panda = document.getElementById('panda');
 
 // Add a click event listener to the button
 button.addEventListener('click', function() {
-    alert("Happy Teddy Day Madam Jiii....! 🎉🐼💖 Hope you have a wonderful day full of love and joy!");
+    alert("Happy Teddy Day Madam jiii....!!!🎉🐼💖 Hope you have a wonderful day full of love and joy!");
 
     // Trigger panda dancing animation
     panda.style.animation = 'pandaDance 1s infinite ease-in-out';
